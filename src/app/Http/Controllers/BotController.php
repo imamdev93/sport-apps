a02d7755e1d5309d3e09202a841a1ebb15083577
Imam Fahmi F <imam.dev93@gmail.com>
2024-02-09 15:25:41 +0700

    remove grouping by team

--- src/app/Http/Controllers/BotController.php
+++ src/app/Http/Controllers/BotController.php
@@ -88,7 +88,14 @@ class BotController extends Controller
                 $responses = '';
                 $members = DB::table(
                     DB::raw(
-                        '(SELECT name,is_member, position, ROW_NUMBER() OVER (PARTITION BY position ORDER BY RAND()) AS row_num FROM users) AS ranked'
+                        '(SELECT 
+                        name,
+                        is_member, 
+                        position, 
+                        char(ascii(row_number() over (PARTITION BY position ORDER BY RAND()))+48) AS alphabet, 
+                        (row_number() over (PARTITION BY position ORDER BY RAND())) AS row_num
+                        FROM users) 
+                        AS ranked'
                     )
                 )
                     ->where('row_num', '<=', $totalTeam)
@@ -99,12 +106,10 @@ class BotController extends Controller
                 $weekOfMonth = Carbon::parse($date)->weekOfMonth;
                 $monthAndYear = Carbon::parse($date)->translatedFormat('F Y');
 
-                $eventName = "Fun Game GW " . $weekOfMonth . " Bulan " . $monthAndYear;
+                $eventName = "Fun Game JDS FC Bulan " . $monthAndYear;
                 if ($weekOfMonth == 1) {
                     $eventName = "Trofeo JDS FC $monthAndYear";
                 }
-
-                $success = '\u2705';
 
                 $responses = $this->formatText('<b>%s</b>' . PHP_EOL, $this->unicodeToUtf8('&#127942;', $eventName));
                 $responses .= $this->formatText('<b>%s</b>' . PHP_EOL, $this->unicodeToUtf8('&#127967;', ' BIS Dragons (https://g.co/kgs/4MqwnS) '));
@@ -117,29 +122,19 @@ class BotController extends Controller
                 }
                 $responses .= $this->formatText('%s' . PHP_EOL, '');
 
-                $teams = [];
-                
-                foreach ($members as $value) {
+                foreach ($members as $key => $value) {
+                    $index = $key + 1;
                     $position = $this->mappingPosition($value->position);
-                    for ($i = 1; $i <= $totalTeam; $i++) {
-                        if ($i == $value->row_num) {
-                            $teams[$i][] = $value->name . ' (' . $position . ')';
-                        }
+                    if (($index % 8) == 1) {
+                        $responses .= $this->formatText('<b>%s</>' . PHP_EOL, 'TIM ' . strtoupper($value->alphabet));
+                        $responses .= $this->formatText('%s' . PHP_EOL, $value->position . '. ' . $value->name . ' (' . $position . ')');
+                    } elseif (($index % 8) != 1) {
+                        $responses .= $this->formatText('%s' . PHP_EOL, $value->position . '. ' . $value->name . ' (' . $position . ')');
                     }
-                }
 
-                $alphabet = ['A', 'B', 'C', 'D', 'E'];
-                
-
-                foreach ($teams as $key => $team) {
-                    $teamName = $alphabet[$key - 1] ?? 'F';
-                    $no = 1;
-                    $responses .= $this->formatText('<b>%s</>' . PHP_EOL, 'TIM ' . $teamName);
-                    foreach ($team as $value) {
-                        $responses .= $this->formatText('%s' . PHP_EOL, $no++ . '. ' . $value);
+                    if (($index % 8) == 0) {
+                        $responses .= $this->formatText('%s' . PHP_EOL, '');
                     }
-                    $responses .= $this->formatText('%s' . PHP_EOL, '');
-                    $no = 0;
                 }
 
                 $players = $totalTeam * 7;
